a5a10b634bc83f5956723f3b1580a9e369b02612
xuxin <943512372@qq.com>
2018-07-18 21:50:44 +0800

    banner

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,18 @@
+require('./index.css');
 require('page/common/header/index.js');
 require('page/common/nav/index.js');
+require('util/slider/index.js');
 let navSide = require('page/common/nav-side/index.js');
+let templateBanner = require('./index.string');
 let _mm = require('util/mm.js');
-navSide.init({
-    name: 'user-center'
-});
+$(function() {
+    let bannerHtml = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml)
+    let $slider = $('.banner').unslider({
+        dots: true
+    });
+    $('.banner-con .banner-arrow').click(function() {
+        let forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    })
+})
